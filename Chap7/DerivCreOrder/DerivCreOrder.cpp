be02f1dc3b67164af4b9ba79f871d147ac4cd9b7
Kim Sein <continue.deve@gmail.com>
2020-07-27 09:34:27 +0900

    :hatched_chick:

---
+++ Chap7/DerivCreOrder/DerivCreOrder.cpp
@@ -0,0 +1,33 @@
+//À¯µµ(derived)
+#include <iostream>
+using namespace std;
+
+class SoBase
+{
+private:
+	int baseNum;
+public:
+	SoBase() : baseNum(20)
+	{
+		cout << "SoBase()" << endl;
+	}
+	SoBase(int n) : baseNum(n)
+	{
+		cout << "SoBase(int n)" << endl;
+	}
+	void ShowBaseData()
+	{
+		cout << baseNum << endl;
+	}
+};
+
+class SoDerived : public SoBase
+{
+private:
+	int derivNum;
+public:
+	SoDerived() : derivNum(30)
+	{
+		cout << "SoDerived()" << endl;
+	}
+};
